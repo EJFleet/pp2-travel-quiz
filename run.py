4767f27dfa481f6304ce46968e987f08dad83d99
Emma Fleetwood <emmafleetwood@gmail.com>
2024-07-22 19:42:16 +0000

    Edit main menu function, add print statements for other functions in order to test

--- run.py
+++ run.py
@@ -28,28 +28,50 @@ def welcome_message():
     Display welcome message to the user with prompt to confirm entry
     """   
     print(f"Welcome to Holiday Budget Tracker!")
-    input(f"Press 'Enter' to continue...")
+    input(f"Press 'Enter' to continue...\n")
 
 # Main Menu
 def main_menu():
     """
     Display Main Menu
     """
-    print("main menu is working")
+    while True:
+
+        print("What would you like to do?")
+        print("  1. Create new holiday budget")
+        print("  2. Update existing budget")
+        print("  3. See budget breakdown")
+        print("  4. Exit program")
+        
+        choice = input("  Enter number 1-4: ").strip()
+        if choice == '1':
+            create_new_budget()
+            break
+        elif choice == '2':
+            add_expense()
+            break
+        elif choice == '3':
+            budget_breakdown()
+            break
+        elif choice == '4':
+            exit_program()
+            break
+        else:
+            print('Invalid number. Please try again')
+
 
 # Create Budget - Title and Total
-main()
-
-# Add Category
-
+def create_new_budget():
+    print("create new budget working")
 
 # Add Expense
-
-
-# Update Existing Budget - Title or Total
-
+def add_expense():
+    print("add expense working")
 
 # See Budget Breakdown
-
+def budget_breakdown():
+    print("budget breakdown working")
 
 # Exit
+def exit_program():
+    print("exit program working")
