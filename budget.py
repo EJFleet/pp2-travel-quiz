fc106d2a480be608bd38cce5526f7fc555dc5ced
Emma Fleetwood <emmafleetwood@gmail.com>
2024-07-25 09:01:01 +0000

    Delete whitespace in budget.py

--- budget.py
+++ budget.py
@@ -3,6 +3,6 @@ class Budget:
     def __init__(self, name=None, amount=0):
         self.name = name if name is not None else "Unnamed"
         self.amount = round(amount, 2)
-    
+
     def __repr__(self):
         return f"Budget: {self.name}, Total: {self.amount}"
