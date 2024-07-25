969126f909056d1046c17827a7adab49d1ce57ff
Emma Fleetwood <emmafleetwood@gmail.com>
2024-07-25 11:56:05 +0000

    Update Budget object

--- budget.py
+++ budget.py
@@ -1,7 +1,7 @@
 class Budget:
 
     def __init__(self, name=None, amount=0):
-        self.name = name if name is not None else "Unnamed"
+        self.name = name if name else "Unnamed"
         self.amount = round(amount, 2)
 
     def __repr__(self):
